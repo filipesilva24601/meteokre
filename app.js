b2a84079054c68a83d949a4cd63f170280a5fda8
Filipe Silva <filipesilva24601@gmail.com>
2021-01-14 11:55:45 +0000

    fixed routing in express (the angular app must be built with --base-href "/client/")

--- app.js
+++ app.js
@@ -18,11 +18,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', express.static(path.join(__dirname, 'client/dist/client/')));
+app.use("/client/", express.static(path.join(__dirname, "client/dist/client")));
 app.use('/users', usersRouter);
 app.use('/api', apiRouter);
+app.use("/client/", function (req, res) {
+  res.sendFile(path.join(__dirname, "client/dist/client/index.html"));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
