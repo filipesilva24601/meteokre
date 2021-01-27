116e0d3eeaf987b8ec5b507aa0b8848a9ddbf25b
Filipe Silva <filipesilva24601@gmail.com>
2021-01-27 11:22:00 +0000

    Make postFile synchronous too.

--- client/src/app/api.service.ts
+++ client/src/app/api.service.ts
@@ -53,70 +53,76 @@ export class ApiService {
                 );
               })
             ),
-        ]).pipe(
-          map(([meta, file]) => {
-            return {
-              fileName: meta.fileName,
-              fileType: meta.fileType,
-              data: file,
-            };
-          }),
-          first()
-        ).toPromise();
+        ])
+          .pipe(
+            map(([meta, file]) => {
+              return {
+                fileName: meta.fileName,
+                fileType: meta.fileType,
+                data: file,
+              };
+            }),
+            first()
+          )
+          .toPromise();
       });
   }
 
-  async postFile(fileBuffer: any, fileName, fileType): Promise<any> {
+  postFile(fileBuffer: any, fileName, fileType) {
     const enc = new TextEncoder();
 
-    const key = await window.crypto.subtle.generateKey(
-      { name: 'AES-GCM', length: 128 },
-      true,
-      ['encrypt', 'decrypt']
-    );
-    const base64key = ab2b64(await window.crypto.subtle.exportKey('raw', key));
+    return window.crypto.subtle
+      .generateKey({ name: 'AES-GCM', length: 128 }, true, [
+        'encrypt',
+        'decrypt',
+      ])
+      .then((key) => {
+        return window.crypto.subtle.exportKey('raw', key).then((rkey) => {
+          const base64key = ab2b64(rkey);
 
-    const iv = window.crypto.getRandomValues(new Uint8Array(12));
+          const iv = window.crypto.getRandomValues(new Uint8Array(12));
 
-    const encryptedMetadata: ArrayBuffer = await window.crypto.subtle.encrypt(
-      { name: 'AES-GCM', iv: iv },
-      key,
-      enc.encode(
-        JSON.stringify({
-          fileName: fileName,
-          fileType: fileType,
-        })
-      )
-    );
+          return window.crypto.subtle
+            .encrypt(
+              { name: 'AES-GCM', iv: iv },
+              key,
+              enc.encode(
+                JSON.stringify({
+                  fileName: fileName,
+                  fileType: fileType,
+                })
+              )
+            )
+            .then((encryptedMetadata) => {
+              return window.crypto.subtle
+                .encrypt({ name: 'AES-GCM', iv: iv }, key, fileBuffer)
+                .then((encryptedData) => {
+                  const form = new FormData();
+                  form.append('file', new Blob([encryptedData]));
 
-    const encryptedData: ArrayBuffer = await window.crypto.subtle.encrypt(
-      { name: 'AES-GCM', iv: iv },
-      key,
-      fileBuffer
-    );
+                  const formMetadata = new FormData();
+                  formMetadata.append('file', new Blob([encryptedMetadata]));
 
-    const form = new FormData();
-    form.append('file', new Blob([encryptedData]));
-
-    const formMetadata = new FormData();
-    formMetadata.append('file', new Blob([encryptedMetadata]));
-
-    return this.http
-      .post(`${this.apiroot}/file/meta`, formMetadata)
-      .pipe(
-        switchMap((res: any) => {
-          return this.http.post(
-            `${this.apiroot}/file/upload/${res.fileid}`,
-            form
-          );
-        }),
-        map((res: any) => {
-          res.encKey = base64key;
-          res.ivKey = ab2b64(iv);
-          return res;
-        }),
-        first()
-      )
-      .toPromise();
+                  return this.http
+                    .post(`${this.apiroot}/file/meta`, formMetadata)
+                    .pipe(
+                      switchMap((res: any) => {
+                        return this.http.post(
+                          `${this.apiroot}/file/upload/${res.fileid}`,
+                          form
+                        );
+                      }),
+                      map((res: any) => {
+                        res.encKey = base64key;
+                        res.ivKey = ab2b64(iv);
+                        return res;
+                      }),
+                      first()
+                    )
+                    .toPromise();
+                });
+            });
+        });
+      });
   }
 }
