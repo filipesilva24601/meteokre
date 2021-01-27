1845a839af94326b7f16b313b12d9c9b8af77766
Filipe Silva <filipesilva24601@gmail.com>
2021-01-27 16:26:06 +0000

    Move key creation duties to Fileuploadstatus and show a status message.

--- client/src/app/api.service.ts
+++ client/src/app/api.service.ts
@@ -68,61 +68,19 @@ export class ApiService {
       });
   }
 
-  postFile(fileBuffer: any, fileName, fileType) {
-    const enc = new TextEncoder();
+  postMeta(encryptedData: ArrayBuffer) {
+    const form = new FormData();
+    form.append('file', new Blob([encryptedData]));
 
-    return window.crypto.subtle
-      .generateKey({ name: 'AES-GCM', length: 128 }, true, [
-        'encrypt',
-        'decrypt',
-      ])
-      .then((key) => {
-        return window.crypto.subtle.exportKey('raw', key).then((rkey) => {
-          const base64key = ab2b64(rkey);
+    return this.http
+      .post(`${this.apiroot}/file/meta`, form);
+  }
 
-          const iv = window.crypto.getRandomValues(new Uint8Array(12));
+  postFile(encryptedData: ArrayBuffer, fileid: string) {
+    const form = new FormData();
+    form.append('file', new Blob([encryptedData]));
 
-          return window.crypto.subtle
-            .encrypt(
-              { name: 'AES-GCM', iv: iv },
-              key,
-              enc.encode(
-                JSON.stringify({
-                  fileName: fileName,
-                  fileType: fileType,
-                })
-              )
-            )
-            .then((encryptedMetadata) => {
-              return window.crypto.subtle
-                .encrypt({ name: 'AES-GCM', iv: iv }, key, fileBuffer)
-                .then((encryptedData) => {
-                  const form = new FormData();
-                  form.append('file', new Blob([encryptedData]));
-
-                  const formMetadata = new FormData();
-                  formMetadata.append('file', new Blob([encryptedMetadata]));
-
-                  return this.http
-                    .post(`${this.apiroot}/file/meta`, formMetadata)
-                    .pipe(
-                      switchMap((res: any) => {
-                        return this.http.post(
-                          `${this.apiroot}/file/upload/${res.fileid}`,
-                          form
-                        );
-                      }),
-                      map((res: any) => {
-                        res.encKey = base64key;
-                        res.ivKey = ab2b64(iv);
-                        return res;
-                      }),
-                      first()
-                    )
-                    .toPromise();
-                });
-            });
-        });
-      });
+    return this.http
+      .post(`${this.apiroot}/file/upload/${fileid}`, form);
   }
 }
--- client/src/app/fileuploadstatus/fileuploadstatus.component.ts
+++ client/src/app/fileuploadstatus/fileuploadstatus.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { catchError, switchMap, tap } from 'rxjs/operators';
 import { ApiService } from '../api.service';
+import { b642ab, ab2b64 } from '../helpers';
 
 @Component({
   selector: '[app-fileuploadstatus]',
@@ -7,11 +9,14 @@ import { ApiService } from '../api.service';
   styleUrls: ['./fileuploadstatus.component.css'],
 })
 export class FileuploadstatusComponent implements OnInit {
+  message: string = 'Not uploaded';
   @Input() file: File;
   @Input() fileName: string;
   fileid: string;
   encKey: string;
   ivKey: string;
+
+  uploading: boolean = false;
 
   constructor(private api: ApiService) {}
 
@@ -25,15 +30,71 @@ export class FileuploadstatusComponent implements OnInit {
   }
 
   uploadFile() {
-    if (this.fileid) {
+    if (this.uploading) {
       return;
     }
-    this.file.arrayBuffer().then((ab) => {
-      this.api.postFile(ab, this.file.name, this.file.type).then((res: any) => {
-        this.fileid = res.fileid;
-        this.encKey = res.encKey;
-        this.ivKey = res.ivKey;
+
+    this.uploading = true;
+
+    const enc = new TextEncoder();
+
+    this.message = 'Generating key';
+    window.crypto.subtle
+      .generateKey({ name: 'AES-GCM', length: 128 }, true, [
+        'encrypt',
+        'decrypt',
+      ])
+      .then((key) => {
+        window.crypto.subtle.exportKey('raw', key).then((rkey) => {
+          this.encKey = ab2b64(rkey);
+
+          const iv = window.crypto.getRandomValues(new Uint8Array(12));
+          this.ivKey = ab2b64(iv);
+
+          window.crypto.subtle
+            .encrypt(
+              { name: 'AES-GCM', iv: iv },
+              key,
+              enc.encode(
+                JSON.stringify({
+                  fileName: this.file.name,
+                  fileType: this.file.type,
+                })
+              )
+            )
+            .then((encryptedMetadata) => {
+              this.message = 'Reading file';
+              this.file
+                .arrayBuffer()
+                .then((ab) => {
+                  this.message = 'Encrypting file';
+                  window.crypto.subtle
+                    .encrypt({ name: 'AES-GCM', iv: iv }, key, ab)
+                    .then((encryptedData) => {
+                      this.message = 'Uploading file';
+                      this.api
+                        .postMeta(encryptedMetadata)
+                        .pipe(
+                          switchMap((res: any) => {
+                            return this.api.postFile(encryptedData, res.fileid);
+                          }),
+                          catchError((err) => {
+                            this.message = 'Failed to upload file.';
+                            throw new Error('');
+                          }),
+                          tap((res: any) => {
+                            this.fileid = res.fileid;
+                            this.message = 'Finished uploading file';
+                          })
+                        )
+                        .toPromise();
+                    });
+                })
+                .finally(() => {
+                  this.uploading = false;
+                });
+            });
+        });
       });
-    });
   }
 }
