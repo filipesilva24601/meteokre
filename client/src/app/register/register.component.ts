0e5dd1a9cebd1ed35f14dfe996034c2c26335097
Filipe Silva <filipesilva24601@gmail.com>
2021-02-05 19:03:44 +0000

    Add error messages to Register page.

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,7 @@
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 import { ApiService } from '../api.service';
 import { UserService } from '../user.service';
 
@@ -12,20 +14,31 @@ export class RegisterComponent implements OnInit {
   constructor(
     private titleService: Title,
     private api: ApiService,
-    private userService: UserService
+    private userService: UserService,
+    private router: Router
   ) {}
 
   username: string;
   password: string;
 
+  message: string;
+
   ngOnInit(): void {
     this.titleService.setTitle('Register');
   }
 
+  clearMessage() {
+    this.message = undefined;
+  }
+
   register() {
-    this.api.register(this.username, this.password).subscribe((res) => {
-      this.userService.loggedIn = true;
-      this.userService.username = this.username;
-    });
+    this.clearMessage();
+    this.api.register(this.username, this.password).subscribe({next: (res:any) => {
+        this.userService.loggedIn = true;
+        this.userService.username = this.username;
+        this.router.navigate(['/pastebin']);
+      }, error: (err: HttpErrorResponse) => {
+        this.message = err.error.message;
+    }});
   }
 }
