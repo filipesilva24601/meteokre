c40e5e4bef5e58836928b9f6f90a97789323f3cb
Filipe Silva <filipesilva24601@gmail.com>
2021-02-05 19:02:37 +0000

    Change DB schema to allow a Google ID.

--- database/db.js
+++ database/db.js
@@ -11,8 +11,10 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
     console.log("Connected to the SQLite database.");
     db.run(`CREATE TABLE user (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name text UNIQUE, 
+            google_id text UNIQUE,
+            name text UNIQUE,
             password text, 
+            CONSTRAINT google_id_unique UNIQUE (google_id),
             CONSTRAINT name_unique UNIQUE (name)
             )`, (err) => {
                 return;
